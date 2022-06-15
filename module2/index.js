8726fbce14768668f644e37c7d7997c747c33df9
Samuel Ujazda <samuelujazda@gmail.com>
2022-06-15 12:34:03 +0200

    saving data to database using nedb

--- module2/index.js
+++ module2/index.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const Datastore = require('nedb');
+
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+
 app.post('/api', (request, response) => {
     console.log('i got a request');
-    console.log(request.body);
     const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
     response.json({
-        status: 'success',
+        status: 'succes',
+        timestamp: timestamp,
         latitude: data.lat,
         longitude: data.lon
-    })
+    });
 });
