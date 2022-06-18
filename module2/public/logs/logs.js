26e9f5c9ee5bb9365b6e7344ab92f9000b133231
Samuel Ujazda <samuelujazda@gmail.com>
2022-06-18 14:01:37 +0200

    file changes

---
+++ module2/public/logs/logs.js
@@ -0,0 +1,25 @@
+getData();
+
+async function getData(){
+    const response = await fetch('/api');
+    const data = await response.json();
+
+    for (item of data) {
+        const root = document.createElement('div');
+        root.classList = 'root';
+        const mood = document.createElement('div');
+        const geo = document.createElement('div');
+        const date = document.createElement('div');
+        const img = document.createElement('img');
+
+        mood.textContent = `mood: ${item.mood}`;
+        geo.textContent = `${item.lat}°, ${item.lon}°`;
+        const dateString = new Date(item.timestamp).toLocaleString();
+        date.textContent = dateString;
+        img.src = item.img64;
+
+        root.append(mood, geo, date, img);
+        document.body.append(root);
+    }
+    console.log(data);
+};
