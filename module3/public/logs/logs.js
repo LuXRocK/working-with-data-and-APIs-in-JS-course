b105f00aa4a7e5600a1865b2bde438f6f493bf89
Samuel Ujazda <samuelujazda@gmail.com>
2022-06-22 11:53:26 +0200

    the project is basicly finished, only have to work on hiding api key

--- module3/public/logs/logs.js
+++ module3/public/logs/logs.js
@@ -11,17 +11,45 @@ async function getData() {
     const data = await response.json();
 
     for (item of data) {
-        const root = document.createElement('div');
-        root.classList = 'root';
 
-        const location = document.createElement('p');
-        location.textContent = `${item.lat}°, ${item.lon}°`;
+        try {
+        
+        const marker = L.marker([item.lat, item.lon]).addTo(map);
+        console.log(item);  
 
-        const time = document.createElement('p');
-        const datestring = new Date(item.timestamp).toLocaleString();
-        time.textContent = datestring
+        const txt = `I'm sitting out here at ${item.lat.toFixed(2)}°, ${item.lon.toFixed(2)}°  
+        on this ${item.weather.description} day and it is ${((item.weather.main.temp)-273.15).toFixed(2)}°C. outside.
+        The concentration of small carcinogenic particles (${item.air.parameter}) 
+        I'm breating in is ${item.air.value} ${item.air.unit}
+        and it was last measured on ${item.air.lastUpdated}`;
 
-        root.append(location, time);
+        marker.bindPopup(txt);
+
+        }catch(error) {
+
+            console.error('something went wrong!');
+
+            const marker = L.marker([item.lat, item.lon]).addTo(map);
+            console.log(item);  
+    
+            const txt = `I'm sitting out here at ${item.lat.toFixed(2)}°, ${item.lon.toFixed(2)}°  
+            on this ${item.weather.description} day and it is ${((item.weather.main.temp)-273.15).toFixed(2)}°C. outside.
+            There is no information about air quality here`;
+    
+            marker.bindPopup(txt);
+
+        }
+        // const root = document.createElement('div');
+        // root.classList = 'root';
+
+        // const location = document.createElement('p');
+        // location.textContent = `${item.lat}°, ${item.lon}°`;
+
+        // const time = document.createElement('p');
+        // const datestring = new Date(item.timestamp).toLocaleString();
+        // time.textContent = datestring
+
+        // root.append(location, time);
         // document.body.append(root);
     }
     console.log(data);
